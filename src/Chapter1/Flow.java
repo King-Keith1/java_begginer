e325127bb8b98d87e8e1d4ec28b32e517aacb350
King-Keith1 <keith4321ben@gmail.com>
2025-09-01 15:05:06 +0200

    commit

---
+++ src/Chapter1/Flow.java
@@ -0,0 +1,16 @@
+package Chapter1;
+
+public class Flow {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 4) {
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x = x + 1;
+        }
+        if (x == 4) {
+            System.out.println("Do");
+        }
+
+    }
+}
