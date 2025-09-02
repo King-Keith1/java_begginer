7b2729786f5ec6d9e1379cbedc4495cbfd84af22
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 10:04:19 +0200

    commit

---
+++ src/Chapter2/DogTestDrive.java
@@ -0,0 +1,9 @@
+package Chapter2;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        DogTestDrive d = new DogTestDrive(); //Creates a Dog object
+        d.size = 40; //Set the size instance variable
+        d.bark(); //Call the bark() method
+    }
+}
