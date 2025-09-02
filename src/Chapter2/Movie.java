ec7339ea57837e931108faa0ede98268113f3b9f
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 10:43:43 +0200

    comiit

--- src/Chapter2/Movie.java
+++ src/Chapter2/Movie.java
@@ -8,4 +8,9 @@ class Movie {
     void playIt() {
         System.out.println("Playing the movie");
     }
+    void display() {
+        System.out.println("title: " + title);
+        System.out.println("genre: " + genre);
+        System.out.println("rating: " + rating);
+    }
 }
