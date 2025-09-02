efaf2b7bada8ea7f46d3f42e566d98910898eee0
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 19:10:44 +0200

    commit

--- src/Chapter2/Exercise_1_Pg_43/DrumKitTestDrive.java
+++ src/Chapter2/Exercise_1_Pg_43/DrumKitTestDrive.java
@@ -1,5 +1,14 @@
 package Chapter2.Exercise_1_Pg_43;
 
 public class DrumKitTestDrive {
-    
+    public static void main(String[] args) {
+        DrumKit d = new DrumKit();
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+
+        d.snare = false;
+        d.playTopHat();
+    }
 }
