a06f0e7e2ae403f5b7db73714f18ce2c20c74be0
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 19:41:44 +0200

    commit

--- src/Chapter2/Exercise_2_Pg_44/EchoTestDrive.java
+++ src/Chapter2/Exercise_2_Pg_44/EchoTestDrive.java
@@ -1,41 +1,20 @@
 package Chapter2.Exercise_2_Pg_44;
 
-//public class EchoTestDrive {
-//    public static void main(String [] args) {
-//        Echo e1 = new Echo();
-//        Echo e2 = new Echo();
-//
-//        int x = 0;
-//        while ( x < 4 ) {
-//            e1.hello();
-//            e1.count = e1.count + 1;
-//
-//            if ( x == 4 ) {
-//                e2.count = e2.count + 1;
-//            }
-//
-//            if ( x < 4 ) {
-//                e2.count = e2.count + e1.count;
-//            }
-//            x = x + 1;
-//        }
-//        System.out.println(e2.count);
-//    }
-//}
-
-//Bouns Question
 public class EchoTestDrive {
     public static void main(String [] args) {
         Echo e1 = new Echo();
-        Echo e2 = e1;
+        Echo e2 = new Echo();
+
         int x = 0;
         while ( x < 4 ) {
             e1.hello();
             e1.count = e1.count + 1;
-            if ( x > 0 ) { // True for x = 1, 2, 3
+
+            if ( x == 4 ) {
                 e2.count = e2.count + 1;
             }
-            if ( x > 1 ) { // True for x = 2, 3
+
+            if ( x < 4 ) {
                 e2.count = e2.count + e1.count;
             }
             x = x + 1;
@@ -43,3 +22,24 @@ public class EchoTestDrive {
         System.out.println(e2.count);
     }
 }
+
+//Bouns Question
+//public class EchoTestDrive {
+//    public static void main(String [] args) {
+//        Echo e1 = new Echo();
+//        Echo e2 = e1;
+//        int x = 0;
+//        while ( x < 4 ) {
+//            e1.hello();
+//            e1.count = e1.count + 1;
+//            if ( x > 0 ) { // True for x = 1, 2, 3
+//                e2.count = e2.count + 1;
+//            }
+//            if ( x > 1 ) { // True for x = 2, 3
+//                e2.count = e2.count + e1.count;
+//            }
+//            x = x + 1;
+//        }
+//        System.out.println(e2.count);
+//    }
+//}
