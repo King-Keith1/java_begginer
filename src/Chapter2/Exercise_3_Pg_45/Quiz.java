136b0a6c3f21e301d64ea3ea73c09ed667bf11b4
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 19:51:31 +0200

    commit

---
+++ src/Chapter2/Exercise_3_Pg_45/Quiz.java
@@ -0,0 +1,29 @@
+package Chapter2.Exercise_3_Pg_45;
+
+//I am compiled from a .java file.
+//Answer: Class
+
+//My instance variable values can be different from my buddy's
+//Answer: Object
+
+//I behave like a template
+//Answer: Class
+
+//I like to do stuff
+//Answer: Method
+
+//I have many methods
+//Class
+
+//I represent "state"
+//Variable
+
+//I have behaviours
+//Object
+
+//I live on the heap.
+//Answer: Object
+
+//I am used to create object instances.
+//Answer: Class
+
