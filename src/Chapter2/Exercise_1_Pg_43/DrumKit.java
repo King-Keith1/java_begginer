7bafded8e295c20e98310ebd357408346d6ba51e
King-Keith1 <keith4321ben@gmail.com>
2025-09-02 19:08:00 +0200

    commit

--- src/Chapter2/Exercise_1_Pg_43/DrumKit.java
+++ src/Chapter2/Exercise_1_Pg_43/DrumKit.java
@@ -1,4 +1,14 @@
 package Chapter2.Exercise_1_Pg_43;
 
 public class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
 }
--- src/Chapter2/Exercise_1_Pg_43/DrumKitTestDrive.java
+++ src/Chapter2/Exercise_1_Pg_43/DrumKitTestDrive.java
@@ -1,4 +1,5 @@
 package Chapter2.Exercise_1_Pg_43;
 
 public class DrumKitTestDrive {
+    
 }
