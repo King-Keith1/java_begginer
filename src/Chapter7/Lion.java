fa0091e0063c50f0c423b725efb0998c7accb84c
King-Keith1 <keith4321ben@gmail.com>
2025-09-11 11:03:20 +0200

    commit

--- src/Chapter7/Lion.java
+++ src/Chapter7/Lion.java
@@ -1,4 +1,15 @@
 package Chapter7;
 
-public class Lion {
+public class Lion extends Animal {
+    public void eat() {
+        System.out.println("Lion is eating.");
+    };
+
+    public void roam() {
+        System.out.println("Lion is roaming.");
+    }
+
+    public void makeNoise() {
+        System.out.println("Lion roars.");
+    }
 }
