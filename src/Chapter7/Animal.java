8af33a1307275503fd86a6b4b57c4638fdda858c
King-Keith1 <keith4321ben@gmail.com>
2025-09-11 11:01:25 +0200

    commit

--- src/Chapter7/Animal.java
+++ src/Chapter7/Animal.java
@@ -1,4 +1,15 @@
 package Chapter7;
 
 public class Animal {
+    public  void eat(){
+        System.out.println("This animal is eating");
+    };
+
+    public void roam(){
+        System.out.println("This animal is roaming around ");
+    };
+
+    public void makeNoise() {
+        System.out.println("This animal is making noise");
+    }
 }
