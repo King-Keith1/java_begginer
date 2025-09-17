5413cb92347a550041f244712231d68176f521c2
King-Keith1 <keith4321ben@gmail.com>
2025-09-17 13:22:38 +0200

    commit

---
+++ src/Chapter9/Car.java
@@ -0,0 +1,15 @@
+package Chapter9;
+
+public class Car {
+    public Car() {
+        // default constructor
+    }
+
+    public Car(String name) {
+        // constructor with String parameter
+    }
+
+    public Car(int size) {
+        // constructor with int parameter
+    }
+}
