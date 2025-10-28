374b578c5f2ae90f9b187034382b30eb4aa9f94a
King-Keith1 <keith4321ben@gmail.com>
2025-10-28 08:17:24 +0200

    commit

--- src/Chapter7/Summary/Zoo.java
+++ src/Chapter7/Summary/Zoo.java
@@ -1,11 +1,13 @@
 package Chapter7.Summary;
 
+// Superclass
 class Animal {
     void makeSound() {
         System.out.println("Some generic animal sound 🐾");
     }
 }
 
+// Subclasses
 class Dog extends Animal {
     @Override
     void makeSound() {
@@ -20,13 +22,16 @@ class Cat extends Animal {
     }
 }
 
+// Test class
 public class Zoo {
     public static void main(String[] args) {
-        Animal a1 = new Dog();  // Polymorphism
+        Animal a1 = new Dog(); // Polymorphism in action
         Animal a2 = new Cat();
 
-        a1.makeSound(); // Calls Dog version
-        a2.makeSound(); // Calls Cat version
+        a1.makeSound(); // Woof! (Dog version)
+        a2.makeSound(); // Meow! (Cat version)
+
+        // Even though both are "Animals", each acts differently
     }
 }
 
